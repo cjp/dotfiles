64d7d426d795e5398d7c92bbf138e3ada6725225
Chris Pilkington <chrispilkington@cvpcorp.com>
2017-06-13 15:15:41 -0400

    Initial commit of .emacs.d/

---
+++ .emacs.d/init.el
@@ -0,0 +1,35 @@
+;;; init.el --- Where all the magic begins
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(defun load-user-file (file)
+  (interactive "f")
+  "Load a file in current user's configuration directory"
+  (load-file (expand-file-name file user-init-dir)))
+
+(defconst user-init-dir
+  (cond ((boundp 'user-emacs-directory)
+         user-emacs-directory)
+        ((boundp 'user-init-directory)
+         user-init-directory)
+        (t "~/.emacs.d/")))
+
+(load-user-file "packages.el")
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+;; load other files `after-init-hook' so all packages are loaded
+(add-hook 'after-init-hook
+ `(lambda ()
+    (load-user-file "bindings.el")
+    (load-user-file "misc.el")
+    (load-user-file "window.el")
+    (load-user-file "spell.el")
+    (load-user-file "evil.el")
+    (when (file-exists-p custom-file)
+      (load custom-file :noerror :nomessage))))
---
+++ .emacs.d/packages.el
@@ -0,0 +1,35 @@
+;; https://stackoverflow.com/questions/10092322/how-to-automatically-install-emacs-packages-by-specifying-a-list-of-package-name
+
+(setq package-archives
+      '(("gnu"         . "http://elpa.gnu.org/packages/")
+        ("melpa"       . "http://melpa.org/packages/")
+        ("marmalade"   . "http://marmalade-repo.org/packages/"))
+      package-archive-priorities
+      '(("melpa" . 20)
+        ("marmalade" . 15)
+        ("gnu" . 10))
+      package-user-dir "~/.emacs.d/elpa")
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+(ensure-package-installed 'hc-zenburn-theme 'magit 'groovy-mode 'markdown-mode 'evil)
+
+;; activate installed packages
+(package-initialize)

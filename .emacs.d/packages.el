17b22bfc25f5cfecee15c5859a8f3bd9de30a88c
Christopher J. Pilkington <chrispilkington@cvpcorp.com>
2017-10-10 11:54:05 -0400

    More emacs changes (#8)
    * Installed ledger

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -30,14 +30,15 @@ Return a list of installed packages or nil for every skipped package."
     (package-refresh-contents))
 
 (ensure-package-installed 'solarized-theme
-			  'hc-zenburn-theme
-			  'magit
-			  'groovy-mode
-			  'markdown-mode
-			  'json-mode
+                          'hc-zenburn-theme
+                          'magit
+                          'groovy-mode
+                          'markdown-mode
+                          'json-mode
                           'go-mode
                           'auto-complete
-			  'evil)
+                          'evil
+                          'ledger-mode)
 
 ;; activate installed packages
 (package-initialize)
